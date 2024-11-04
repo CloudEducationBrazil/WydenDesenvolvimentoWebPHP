d491a090070b1b1f615dc717ea8aa640affd79df
Heleno Cardoso <helenocardosofilho@gmail.com>
2024-11-04 13:01:43 -0300

    Dev PHP Web Ajustesss 04/11/2024 2024.2

---
+++ 02 Exercises/04 PHP/10 Login/pagF_Cad.php
@@ -0,0 +1,9 @@
+<?php
+  $sLogin = $_GET["login"];
+  $sSenha = $_GET["senha"];
+
+// String de Conexão com o BD
+// Tipo de Transação
+// https://www.youtube.com/watch?v=NIzjRyRmQxU
+
+  echo "Login: " . $sLogin . "<br/> Senha:" . $sSenha;
